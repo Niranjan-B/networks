29613377c2c02b10c056ae72824c5f92a3de2001
Niranjan-B <nero.niranjan@gmail.com>
2017-04-05 17:28:07 -0700

    reading from a file and processing each line to make the size of each operand to 10 bits and concatenating packet number to each line from 0 to 99

--- client.c
+++ client.c
@@ -4,11 +4,85 @@
 #include<strings.h>
 #include<stdbool.h>
 
+char numOneBuffer[10];
+char numTwoBuffer[10];
+char numBuffer[20];
+char buffer[40];
+int numBufferIndex = 0;
+bool commaFlag = false;
+
+
+void clearBuffer() {
+	bzero(&numOneBuffer, 10);
+	bzero(&numTwoBuffer, 10);
+	bzero(&numBuffer, 20);
+	bzero(&buffer, 40);
+	commaFlag = false;
+	numBufferIndex = 0;
+}
+
+void storeSecondNum(char *num, int size) {
+	int i=0;
+	for (i=0; i<(10 - size); i++) {
+		numTwoBuffer[i] = '0';
+	}
+	while(*num) {
+		numTwoBuffer[i] = *num++;
+		i++;
+	}
+	//printf("second num = %s\n", numTwoBuffer);
+}
+
+void storeFirstNum(char *num, int size) {
+	int i=0;
+	for (i=0; i<(10 - size); i++) {
+		numOneBuffer[i] = '0';
+	}
+	while(*num) {
+		numOneBuffer[i] = *num++;
+		i++;
+	}
+	//printf("first num = %s\n", numOneBuffer);
+}
+
+void paddZeros(char temp[]) {
+	// splitting string based on ,
+	char *resultToken;
+	char *result;
+	char *rest = temp;
+	int count = 0;
+
+	while ((resultToken = strtok_r(rest, ",", &rest))) {
+			if (count == 0) {
+				storeFirstNum(resultToken, strlen(resultToken));
+			} else {
+				storeSecondNum(resultToken, strlen(resultToken));
+			}
+			count++;
+	}
+
+}
+
+int storePaddedNumbersIntoBuffer(int bufferIndex) {
+	buffer[bufferIndex++] = ',';
+	// storing first number
+	int i=0;
+	for (i=0; i < 10; i++) {
+		buffer[bufferIndex++] = numOneBuffer[i];
+	}
+	i = 0;
+	buffer[bufferIndex++] = ',';
+	// storing second number
+	for (i=0; i < 10; i++) {
+		buffer[bufferIndex++] = numTwoBuffer[i];
+	}
+	return bufferIndex;
+}
+
 int main() {
-	int index = 1;
-	int bufferIndex = 0;
-	char buffer[40];
+	int index = 0;
 	bool flag = true;
+	int bufferIndex = 0;
 
 	FILE *fp;
 	int c;
@@ -17,17 +91,30 @@ int main() {
 	while((c=fgetc(fp)) != EOF) {
 		if (c == '\n' && flag) {
 			flag = false;
+			
+			paddZeros(numBuffer);
+			bufferIndex = storePaddedNumbersIntoBuffer(bufferIndex);
+
 			buffer[bufferIndex++] = ',';
-			//buffer[bufferIndex]='1';
 			sprintf(&buffer[bufferIndex], "%d", index);
 			printf("%s", buffer);
 			index++;
+			clearBuffer();
 			bufferIndex = 0;
-			bzero(&buffer, 40);
 			printf("\n");
 		} else {
+			if (c == ',') {
+				commaFlag = true;
+				numBuffer[numBufferIndex++] = c;
+			} else {
+				if (!commaFlag) {
+					buffer[bufferIndex++] = c;
+				} else {
+					numBuffer[numBufferIndex++] = c;
+				}
+			}
+
 			flag = true;
-			buffer[bufferIndex++] = c;
 		}
 	}
 	fclose(fp);
